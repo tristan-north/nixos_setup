8eb7078675b1edcb8dae2b0f06e7abe61a75acb6
Tristan North <git@tristan-north.com>
2026-01-01 15:30:12 +1100

    android emulator stuff

--- configuration.nix
+++ configuration.nix
@@ -19,12 +19,20 @@ in
   boot.kernelPackages = pkgs.linuxPackages_6_17;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # Clean up the boot process so the login prompt looks nicer
+  # This hides kernel logs on boot so tuigreet is the first thing you see
+  boot.kernelParams = [ "quiet" "splash" ];
 
   zramSwap = {
     enable = true;
     memoryMax = 4 * 1024 * 1024 * 1024; # 4Gig
   };
 
+
+  services.udev.extraRules = ''
+    # Disable Touchscreen to stop phantom touches
+    ACTION=="add|change", KERNEL=="event*", ATTRS{name}=="quicki2c-hid 27C6:012D", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+  '';
 
   networking.hostName = "nixos"; # Define your hostname.
 
@@ -34,6 +42,33 @@ in
   hardware.bluetooth.enable = true;
 
   services.upower.enable = true; # Battery features, needed by quickshell
+
+  # DNLA media serving
+  services.minidlna.enable = true;
+  services.minidlna.openFirewall = true;
+  services.minidlna.settings = {
+    friendly_name = "NixOS Media";
+    media_dir = [ "V,/mnt/media-share" ];
+    inotify = "yes";  # Auto update the library
+    log_level = "error";
+  };
+  users.users.minidlna = {
+    extraGroups = [ "users" ]; # so minidlna can access the files.
+  };
+  # For DNLA to access the Videos dir, since it has trouble
+  # accessing from within my home dir.
+  systemd.tmpfiles.rules = [
+    "d /mnt/media-share 0755 tristan users -"
+  ];
+  systemd.mounts = [{
+    what = "/home/tristan/Videos";
+    where = "/mnt/media-share";
+    type = "none";
+    options = "bind";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "minidlna.service" ];
+    requiredBy = [ "minidlna.service" ];
+  }];
 
   # Flatpak setup
   services.flatpak.enable = true;
@@ -75,7 +110,7 @@ in
   users.users.tristan = {
     isNormalUser = true;
     description = "Tristan";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "kvm" "adbusers"];
     packages = with pkgs; [];
   };
 
@@ -84,26 +119,8 @@ in
 
   hardware.graphics.enable = true;
 
-  # For running in vmware
-  #virtualisation.vmware.guest.enable = true;
- # environment.sessionVariables = {
- #   WLR_RENDERER = "pixman";
- #   WLR_RENDERER_ALLOW_SOFTWARE = "1";
-#    WLR_NO_HARDWARE_CURSORS = "1";
-
-#    LIBGL_ALWAYS_SOFTWARE = "1";
-    # MESA_LOADER_DRIVER_OVERRIDE = "llvmpipe";
-    # GALLIUM_DRIVER = "llvmpipe";
-
-    # WLR_DRM_NO_ATOMIC = "1";
-#  };
-
   # Enable flakes and the new command line tool
   nix.settings.experimental-features = ["nix-command"];
-
-  # Auto login to TTY, if end up changing to use a display manager
-  # gdm/sddm/lightdm/greetd can probably remove this.
-  services.getty.autologinUser = "tristan";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -122,7 +139,15 @@ in
   brightnessctl # Monitor brightness controller
   wlsunset # Nightlight
   gnome-themes-extra   # provides Adwaita-dark
-  unstable.flutter
+  # unstable.flutter
+  lazygit
+  yazi
+  fzf
+  starship
+  swaylock
+  swayidle
+  unstable.opencode
+  xwayland-satellite # Niri X11 support, for eg android emulator
 
   # Neovim stuff
   tree-sitter
@@ -141,7 +166,23 @@ in
     };
   };
 
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+  services.mullvad-vpn.enable = true;
+
   programs.niri.enable = true;
+
+  # Enable Greetd
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        # Use tuigreet.
+        # --time --cmd niri-session: Tells it to show time and run niri after login
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+        user = "greeter";
+      };
+    };
+  };
 
   # Hint electron apps to use wayland
   environment.variables.ELECTRON_OZONE_PLATFORM_HINT = "wayland";
